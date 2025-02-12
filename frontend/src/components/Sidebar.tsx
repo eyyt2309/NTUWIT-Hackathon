9db100cfee6e092e7b14d3d7e1fc256b58bfd981
enjiaaaa <161003075+enjiaaaa@users.noreply.github.com>
2025-02-13 00:56:03 +0800

    css update #1
    1. replace index css with app css then delete index css
    2. completed sidebar css

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -4,71 +4,69 @@ import "../css/Sidebar.css";
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="d-flex">
-        <button className="toggle-btn" type="button">
-          <img
-            src="/src/assets/logo_white.png"
-            alt="whiteLogo"
-            className="menu-icon"
-          />
-        </button>
-        <div className="sidebar-logo">
-          <Link to="/">! Hard Coders</Link>
-        </div>
+      <button className="toggle-btn" type="button">
+        <img
+          src="/src/assets/logo_white.png"
+          alt="whiteLogo"
+          className="menu-icon"
+        />
 
-        {/* Sidebar Navigation Links */}
-        <ul className="sidebar-nav">
-          <li className="sidebar-item">
-            <Link to="/dashboard" className="sidebar-link">
-              <i className="lni lni-dashboard-square-1"></i>
-              <span>DashBoard</span>
-            </Link>
-          </li>
-          <li className="sidebar-item">
-            <Link to="/courses" className="sidebar-link">
-              <i className="lni lni-pen-to-square"></i>
-              <span>Courses</span>
-            </Link>
-          </li>
-          <li className="sidebar-item">
-            <Link to="/projects" className="sidebar-link">
-              <i className="lni lni-user-4"></i>
-              <span>Projects</span>
-            </Link>
-          </li>
-          <li className="sidebar-item">
-            <Link to="/profile" className="sidebar-link">
-              <i className="lni lni-user-4"></i>
-              <span>Profile</span>
-            </Link>
-          </li>
-          <li className="sidebar-item">
-            <Link to="/community-upload" className="sidebar-link">
-              <i className="lni lni-upload-1"></i>
-              <span>Community Upload</span>
-            </Link>
-          </li>
-          <li className="sidebar-item">
-            <Link to="/collaborate" className="sidebar-link">
-              <i className="lni lni-user-multiple-4"></i>
-              <span>Collaborate</span>
-            </Link>
-          </li>
-        </ul>
+      </button>
+      <div className="sidebar-logo">
+        <Link to="/">! Hard Coders</Link>
+      </div>
 
-        {/* Sidebar Footer */}
-        <div className="sidebar-footer">
-          <Link to="/settings" className="sidebar-link">
-            <i className="lni lni-gear-1"></i>
-            <span>Settings</span>
+      {/* Sidebar Navigation Links */}
+      <ul className="sidebar-nav">
+        <li className="sidebar-item">
+          <Link to="/dashboard" className="sidebar-link">
+            <i className="lni lni-dashboard-square-1"></i>
+            <span>DashBoard</span>
           </Link>
-        </div>
-        <div className="sidebar-footer">
-          <Link to="/" className="sidebar-link">
-            <i className="lni lni-exit"></i>
-            <span>Logout</span>
+        </li>
+        <li className="sidebar-item">
+          <Link to="/courses" className="sidebar-link">
+            <i className="lni lni-pen-to-square"></i>
+            <span>Courses</span>
           </Link>
-        </div>
+        </li>
+        <li className="sidebar-item">
+          <Link to="/projects" className="sidebar-link">
+            <i className="lni lni-user-4"></i>
+            <span>Projects</span>
+          </Link>
+        </li>
+        <li className="sidebar-item">
+          <Link to="/profile" className="sidebar-link">
+            <i className="lni lni-user-4"></i>
+            <span>Profile</span>
+          </Link>
+        </li>
+        <li className="sidebar-item">
+          <Link to="/community-upload" className="sidebar-link">
+            <i className="lni lni-upload-1"></i>
+            <span>Community Upload</span>
+          </Link>
+        </li>
+        <li className="sidebar-item">
+          <Link to="/collaborate" className="sidebar-link">
+            <i className="lni lni-user-multiple-4"></i>
+            <span>Collaborate</span>
+          </Link>
+        </li>
+      </ul>
+
+      {/* Sidebar Footer */}
+      <div className="sidebar-footer">
+        <Link to="/settings" className="sidebar-link">
+          <i className="lni lni-gear-1"></i>
+          <span>Settings</span>
+        </Link>
+
+        <Link to="/" className="sidebar-link">
+          <i className="lni lni-exit"></i>
+          <span>Logout</span>
+        </Link>
       </div>
     </div>
   );
