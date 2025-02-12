5caf21a393bbf312b7b56d884656c199429f3a33
eyyt2309 <ethan97590773@gmail.com>
2025-02-12 20:28:16 +0800

    fixed sidebar enjia pls fix the css it looks like dog

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -15,86 +15,86 @@ function Sidebar() {
         <div className="sidebar-logo">
           <Link to="/">! Hard Coders</Link>
         </div>
-      </div>
 
-      {/* Sidebar Navigation Links */}
-      <ul className="sidebar-nav">
-        <li className="sidebar-item">
-          <Link to="/dashboard" className="sidebar-link">
-            <i className="lni lni-dashboard-square-1"></i>
-            <span>DashBoard</span>
-          </Link>
-        </li>
-        <li className="sidebar-item">
-          <Link to="/courses" className="sidebar-link">
-            <i className="lni lni-pen-to-square"></i>
-            <span>Courses</span>
-          </Link>
-        </li>
+        {/* Sidebar Navigation Links */}
+        <ul className="sidebar-nav">
+          <li className="sidebar-item">
+            <Link to="/dashboard" className="sidebar-link">
+              <i className="lni lni-dashboard-square-1"></i>
+              <span>DashBoard</span>
+            </Link>
+          </li>
+          <li className="sidebar-item">
+            <Link to="/courses" className="sidebar-link">
+              <i className="lni lni-pen-to-square"></i>
+              <span>Courses</span>
+            </Link>
+          </li>
 
-        {/* Projects Dropdown */}
-        <li className="sidebar-item">
-          <a
-            href="#"
-            className="sidebar-link collapsed has-dropdown"
-            data-bs-toggle="collapse"
-            data-bs-target="#projectsDropdown"
-            aria-expanded="false"
-            aria-controls="projectsDropdown"
-          >
-            <i className="lni lni-books-2"></i>
-            <span>Projects</span>
-          </a>
-          <ul
-            id="projectsDropdown"
-            className="sidebar-dropdown list-unstyled collapse"
-            data-bs-parent="#sidebar"
-          >
-            <li className="sidebar-item">
-              <Link to="/projects/base" className="sidebar-link">
-                Base
-              </Link>
-            </li>
-            <li className="sidebar-item">
-              <Link to="/projects/community" className="sidebar-link">
-                Community
-              </Link>
-            </li>
-          </ul>
-        </li>
+          {/* Projects Dropdown */}
+          <li className="sidebar-item">
+            <a
+              href="#"
+              className="sidebar-link collapsed has-dropdown"
+              data-bs-toggle="collapse"
+              data-bs-target="#projectsDropdown"
+              aria-expanded="false"
+              aria-controls="projectsDropdown"
+            >
+              <i className="lni lni-books-2"></i>
+              <span>Projects</span>
+            </a>
+            <ul
+              id="projectsDropdown"
+              className="sidebar-dropdown list-unstyled collapse"
+              data-bs-parent="#sidebar"
+            >
+              <li className="sidebar-item">
+                <Link to="/projects/base" className="sidebar-link">
+                  Base
+                </Link>
+              </li>
+              <li className="sidebar-item">
+                <Link to="/projects/community" className="sidebar-link">
+                  Community
+                </Link>
+              </li>
+            </ul>
+          </li>
 
-        <li className="sidebar-item">
-          <Link to="/profile" className="sidebar-link">
-            <i className="lni lni-user-4"></i>
-            <span>Profile</span>
-          </Link>
-        </li>
-        <li className="sidebar-item">
-          <Link to="/community-upload" className="sidebar-link">
-            <i className="lni lni-upload-1"></i>
-            <span>Community Upload</span>
-          </Link>
-        </li>
-        <li className="sidebar-item">
-          <Link to="/collaborate" className="sidebar-link">
-            <i className="lni lni-user-multiple-4"></i>
-            <span>Collaborate</span>
-          </Link>
-        </li>
-      </ul>
+          <li className="sidebar-item">
+            <Link to="/profile" className="sidebar-link">
+              <i className="lni lni-user-4"></i>
+              <span>Profile</span>
+            </Link>
+          </li>
+          <li className="sidebar-item">
+            <Link to="/community-upload" className="sidebar-link">
+              <i className="lni lni-upload-1"></i>
+              <span>Community Upload</span>
+            </Link>
+          </li>
+          <li className="sidebar-item">
+            <Link to="/collaborate" className="sidebar-link">
+              <i className="lni lni-user-multiple-4"></i>
+              <span>Collaborate</span>
+            </Link>
+          </li>
+        </ul>
 
-      {/* Sidebar Footer */}
-      <div className="sidebar-footer">
-        <Link to="/settings" className="sidebar-link">
-          <i className="lni lni-gear-1"></i>
-          <span>Settings</span>
-        </Link>
-      </div>
-      <div className="sidebar-footer">
-        <Link to="/" className="sidebar-link">
-          <i className="lni lni-exit"></i>
-          <span>Logout</span>
-        </Link>
+        {/* Sidebar Footer */}
+        <div className="sidebar-footer">
+          <Link to="/settings" className="sidebar-link">
+            <i className="lni lni-gear-1"></i>
+            <span>Settings</span>
+          </Link>
+        </div>
+        <div className="sidebar-footer">
+          <Link to="/" className="sidebar-link">
+            <i className="lni lni-exit"></i>
+            <span>Logout</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
