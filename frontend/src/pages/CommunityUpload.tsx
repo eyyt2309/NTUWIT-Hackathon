ca276a6c858475f97bb9c44286f12ed2abadb12a
eyyt2309 <ethan97590773@gmail.com>
2025-02-12 23:52:45 +0800

    added sidebar, search bar, added headers for all pages + routing

--- frontend/src/pages/CommunityUpload.tsx
+++ frontend/src/pages/CommunityUpload.tsx
@@ -1,4 +1,62 @@
+import { useState } from "react";
+import "../css/CommunityUpload.css";
+
 function CommunityUpload() {
-  return <div className="upload-container"></div>;
+  const [isPrivate, setIsPrivate] = useState(false);
+  return (
+    <div className="upload-container">
+      <div className="form-container">
+        <form>
+          <div className="form-group">
+            <label>Input Title:</label>
+            <input type="text" />
+          </div>
+
+          <div className="form-group">
+            <label>Input Problem Statement:</label>
+            <input type="text" />
+          </div>
+
+          <div className="form-group">
+            <label>Constraints:</label>
+            <input type="text" />
+          </div>
+
+          <div className="form-group">
+            <label>Time Limit:</label>
+            <input type="text" />
+          </div>
+
+          <div className="form-group">
+            <label>Sample Input:</label>
+            <input type="text" />
+          </div>
+
+          <div className="form-group">
+            <label>Sample Output:</label>
+            <input type="text" />
+          </div>
+
+          <div className="form-group">
+            <label>Explanation:</label>
+            <input type="text" />
+          </div>
+
+          {/* Toggle Switch */}
+          <div className="form-group toggle-group">
+            <label>Set As Private</label>
+            <div
+              className="toggle-switch"
+              onClick={() => setIsPrivate(!isPrivate)}
+            >
+              <span className={`toggle-slider ${isPrivate ? "on" : "off"}`}>
+                {isPrivate ? "ON" : "OFF"}
+              </span>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 export default CommunityUpload;
