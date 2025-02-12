a41ad3ea62d8e33ed79e9970b9f2a170df72a316
eyyt2309 <ethan97590773@gmail.com>
2025-02-12 19:56:29 +0800

    fixed routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,38 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Courses from "./pages/Courses";
+import ProjectsBase from "./pages/ProjectsBase";
+import ProjectsCommunity from "./pages/ProjectsCommunity";
+import Profile from "./pages/Profile";
+import CommunityUpload from "./pages/CommunityUpload";
+import Collaborate from "./pages/Collaborate";
+import Settings from "./pages/Settings";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <>
-      <Dashboard />
+      <Router>
+        <div className="d-flex">
+          <Sidebar />
+          <div className="container-fluid p-4">
+            <Routes>
+              <Route path="/Dashboard" element={<Dashboard />} />
+              <Route path="/courses" element={<Courses />} />
+              <Route path="/projects/base" element={<ProjectsBase />} />
+              <Route
+                path="/projects/community"
+                element={<ProjectsCommunity />}
+              />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/community-upload" element={<CommunityUpload />} />
+              <Route path="/collaborate" element={<Collaborate />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
     </>
   );
 }
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,105 +1,9 @@
-// import { Link } from "react-router-dom";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "../css/Sidebar.css";
-
-// function Sidebar() {
-//   return (
-//     <aside id="sidebar">
-//       <div className="d-flex">
-//         <button className="toggle-btn" type="button">
-//           <img
-//             src="/src/assets/logo_white.png"
-//             alt="whiteLogo"
-//             className="menu-icon"
-//           />
-//         </button>
-//         <div className="sidebar-logo">
-//           <Link to="/">! Hard Coders</Link>
-//         </div>
-//       </div>
-//       <ul className="sidebar-nav">
-//         <li className="sidebar-item">
-//           <Link to="/dashboard" className="sidebar-link">
-//             <i className="lni lni-dashboard-square-1"></i>
-//             <span>DashBoard</span>
-//           </Link>
-//         </li>
-//         <li className="sidebar-item">
-//           <Link to="/courses" className="sidebar-link">
-//             <i className="lni lni-pen-to-square"></i>
-//             <span>Courses</span>
-//           </Link>
-//         </li>
-//         <li className="sidebar-item">
-//           <a
-//             href="#"
-//             className="sidebar-link collapsed has-dropdown"
-//             data-bs-toggle="collapse"
-//             data-bs-target="#projectsDropdown"
-//             aria-expanded="false"
-//             aria-controls="projectsDropdown"
-//           >
-//             <i className="lni lni-books-2"></i>
-//             <span>Projects</span>
-//           </a>
-//           <ul
-//             id="projectsDropdown"
-//             className="sidebar-dropdown list-unstyled collapse"
-//             data-bs-parent="#sidebar"
-//           >
-//             <li className="sidebar-item">
-//               <Link to="/projects/base" className="sidebar-link">
-//                 Base
-//               </Link>
-//             </li>
-//             <li className="sidebar-item">
-//               <Link to="/projects/community" className="sidebar-link">
-//                 Community
-//               </Link>
-//             </li>
-//           </ul>
-//         </li>
-//         <li className="sidebar-item">
-//           <Link to="/profile" className="sidebar-link">
-//             <i className="lni lni-user-4"></i>
-//             <span>Profile</span>
-//           </Link>
-//         </li>
-//         <li className="sidebar-item">
-//           <Link to="/community-upload" className="sidebar-link">
-//             <i className="lni lni-upload-1"></i>
-//             <span>Community Upload</span>
-//           </Link>
-//         </li>
-//         <li className="sidebar-item">
-//           <Link to="/collaborate" className="sidebar-link">
-//             <i className="lni lni-user-multiple-4"></i>
-//             <span>Collaborate</span>
-//           </Link>
-//         </li>
-//       </ul>
-//       <div className="sidebar-footer">
-//         <Link to="/settings" className="sidebar-link">
-//           <i className="lni lni-gear-1"></i>
-//           <span>Settings</span>
-//         </Link>
-//       </div>
-//       <div className="sidebar-footer">
-//         <Link to="/" className="sidebar-link">
-//           <i className="lni lni-exit"></i> <span>Logout</span>
-//         </Link>
-//       </div>
-//     </aside>
-//   );
-// }
-
-// export default Sidebar;
-
 import { Link } from "react-router-dom";
+import "../css/Sidebar.css";
 
 function Sidebar() {
   return (
-    <aside id="sidebar">
+    <div className="sidebar">
       <div className="d-flex">
         <button className="toggle-btn" type="button">
           <img
@@ -112,14 +16,88 @@ function Sidebar() {
           <Link to="/">! Hard Coders</Link>
         </div>
       </div>
+
+      {/* Sidebar Navigation Links */}
       <ul className="sidebar-nav">
         <li className="sidebar-item">
           <Link to="/dashboard" className="sidebar-link">
-          <i className=""
+            <i className="lni lni-dashboard-square-1"></i>
+            <span>DashBoard</span>
+          </Link>
+        </li>
+        <li className="sidebar-item">
+          <Link to="/courses" className="sidebar-link">
+            <i className="lni lni-pen-to-square"></i>
+            <span>Courses</span>
+          </Link>
+        </li>
+
+        {/* Projects Dropdown */}
+        <li className="sidebar-item">
+          <a
+            href="#"
+            className="sidebar-link collapsed has-dropdown"
+            data-bs-toggle="collapse"
+            data-bs-target="#projectsDropdown"
+            aria-expanded="false"
+            aria-controls="projectsDropdown"
+          >
+            <i className="lni lni-books-2"></i>
+            <span>Projects</span>
+          </a>
+          <ul
+            id="projectsDropdown"
+            className="sidebar-dropdown list-unstyled collapse"
+            data-bs-parent="#sidebar"
+          >
+            <li className="sidebar-item">
+              <Link to="/projects/base" className="sidebar-link">
+                Base
+              </Link>
+            </li>
+            <li className="sidebar-item">
+              <Link to="/projects/community" className="sidebar-link">
+                Community
+              </Link>
+            </li>
+          </ul>
+        </li>
+
+        <li className="sidebar-item">
+          <Link to="/profile" className="sidebar-link">
+            <i className="lni lni-user-4"></i>
+            <span>Profile</span>
+          </Link>
+        </li>
+        <li className="sidebar-item">
+          <Link to="/community-upload" className="sidebar-link">
+            <i className="lni lni-upload-1"></i>
+            <span>Community Upload</span>
+          </Link>
+        </li>
+        <li className="sidebar-item">
+          <Link to="/collaborate" className="sidebar-link">
+            <i className="lni lni-user-multiple-4"></i>
+            <span>Collaborate</span>
           </Link>
         </li>
       </ul>
-    </aside>
+
+      {/* Sidebar Footer */}
+      <div className="sidebar-footer">
+        <Link to="/settings" className="sidebar-link">
+          <i className="lni lni-gear-1"></i>
+          <span>Settings</span>
+        </Link>
+      </div>
+      <div className="sidebar-footer">
+        <Link to="/" className="sidebar-link">
+          <i className="lni lni-exit"></i>
+          <span>Logout</span>
+        </Link>
+      </div>
+    </div>
   );
 }
+
 export default Sidebar;
