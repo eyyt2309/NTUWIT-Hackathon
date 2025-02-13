0ecf23320ef24342e648cd56ab73d3a8e384bc06
enjiaaaa <161003075+enjiaaaa@users.noreply.github.com>
2025-02-13 20:41:03 +0800

    css uodate 5
    collaboration page

--- frontend/src/pages/Collaborate.tsx
+++ frontend/src/pages/Collaborate.tsx
@@ -1,34 +1,33 @@
+import "../css/Collaborate.css";
+
 function Collaborate() {
   const groups = [
     {
-      title: "SC3000 SCSB",
-      description: "Learning the Foundations of Machine Learning",
-      status: "ongoing",
+      title: "SC3000 TG20",
+      status: "Current",
     },
     {
-      title: "SC3010 SCSB",
-      description: "Importance of Security",
-      status: "ongoing",
+      title: "SC3010 TG01",
+      status: "Past",
+    },
+  ];
 
-    }
-  ]
-  return <div className="collaborate-page">
-    <div className="group-grid">
-      {groups.map((group, index) => (
-        <div key={index} className={`group-card ${group.status}`}>
-          <h3>{group.title}</h3>
-          <p>{group.description}</p>
-          <span className={`status ${group.status}`}>{group.status}</span>
-        </div>
-      ))}
+  return (
+    <div className="collaborate-page">
+      <div className="header-container">
+        <header>Groups</header>
+        <button className="join-group-btn">Join Group</button>
+      </div>
+      <div className="groups-grid">
+        {groups.map((group, index) => (
+          <div key={index} className={`group-card ${group.status}`}>
+            <h3>{group.title}</h3>
+            <span className={`status ${group.status}`}>{group.status}</span>
+          </div>
+        ))}
+      </div>
     </div>
-    <h1>Groups</h1>
-    <button className="joinGroup">Join Group</button>
-
-
-    <div className="team-container">
-
-    </div>
-  </div>;
+  );
 }
+
 export default Collaborate;
