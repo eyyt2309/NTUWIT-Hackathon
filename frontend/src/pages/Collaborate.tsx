345221c22b26fb6395c65c7fe2d502e3f262b4c6
enjiaaaa <161003075+enjiaaaa@users.noreply.github.com>
2025-02-13 19:59:17 +0800

    Update Collaborate.tsx

--- frontend/src/pages/Collaborate.tsx
+++ frontend/src/pages/Collaborate.tsx
@@ -1,4 +1,30 @@
 function Collaborate() {
-  return <div></div>;
+  const groups = [
+    {
+      title: "SC3000 SCSB",
+      description: "Learning the Foundations of Machine Learning"
+    }
+    {
+      title: "SC3010 SCSB",
+      description: "Importance of Security"
+    }
+  ]
+  return <div className="project-team">
+    <h1>Groups</h1>
+    <button className="joinGroup">Join Group</button>
+    <div className="projects-grid">
+      {groups.map((group, index) => (
+        <div key={index} className={`project-card ${project.status}`}>
+          <h3>{project.title}</h3>
+          <p>{project.tech}</p>
+          <span className={`status ${project.status}`}>{project.status}</span>
+        </div>
+      ))}
+    </div>
+
+    <div className="team-container">
+
+    </div>
+  </div>;
 }
 export default Collaborate;
