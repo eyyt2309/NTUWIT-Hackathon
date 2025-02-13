e9d02e5fd2b438a8d43e8940b5af63cf1b7acfcc
eyyt2309 <ethan97590773@gmail.com>
2025-02-13 17:05:24 +0800

    added recent projects to dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,16 @@
+import RecentProjects from "../components/RecentProjects";
 import "../css/Dashboard.css";
+import { useEffect, useState } from "react";
 
 function Dashboard() {
   return (
     <div className="dashboard_container">
-      <div className="navbar"></div>
+      <div className="recent+leader-container">
+        <RecentProjects />
+        <div className="leaderboard-container"></div>
+      </div>
+      <div className="dailychallenge"></div>
+      <div className="project-container"></div>
     </div>
   );
 }
