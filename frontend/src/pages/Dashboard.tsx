fa577909ce1eda2937aa1719fef7246bb8fd97de
CLJR <calvinleejiarong@gmail.com>
2025-02-14 01:56:26 +0800

    dashboard semi works

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -2,19 +2,26 @@ import ProjectHUD from "../components/ProjectHUD";
 import RecentProjects from "../components/RecentProjects";
 import "../css/Dashboard.css";
 import { useEffect, useState } from "react";
+import Sidebar from "../components/Sidebar";
+import Searchbar from "../components/Searchbar";
 
 function Dashboard() {
   const [userId, setUserId] = useState<string | null>(null);
 
   useEffect(() => {
     setUserId(sessionStorage.getItem("userId"));
-  }, []); // Added missing dependency array
+  }, []);
   console.log(userId);
   return (
-    <div className="dashboard_container">
-      <ProjectHUD />
-      <RecentProjects />
-    </div>
+    <>
+      <Sidebar />
+      <Searchbar />
+
+      <div className="dashboard_container">
+        <ProjectHUD />
+        <RecentProjects />
+      </div>
+    </>
   );
 }
 
