e85ba0121998d22a2a644d3d9821c3bfd9bf73df
eyyt2309 <ethan97590773@gmail.com>
2025-02-13 20:08:15 +0800

    fixed collaborate

--- frontend/src/pages/Collaborate.tsx
+++ frontend/src/pages/Collaborate.tsx
@@ -9,26 +9,24 @@ function Collaborate() {
       title: "SC3010 SCSB",
       description: "Importance of Security",
       status: "ongoing",
+    },
+  ];
+  return (
+    <div className="collaborate-page">
+      <div className="group-grid">
+        {groups.map((group, index) => (
+          <div key={index} className={`group-card ${group.status}`}>
+            <h3>{group.title}</h3>
+            <p>{group.description}</p>
+            <span className={`status ${group.status}`}>{group.status}</span>
+          </div>
+        ))}
+      </div>
+      <h1>Groups</h1>
+      <button className="joinGroup">Join Group</button>
 
-    }
-  ]
-  return <div className="collaborate-page">
-    <div className="group-grid">
-      {groups.map((group, index) => (
-        <div key={index} className={`group-card ${group.status}`}>
-          <h3>{group.title}</h3>
-          <p>{group.description}</p>
-          <span className={`status ${group.status}`}>{group.status}</span>
-        </div>
-      ))}
+      <div className="team-container"></div>
     </div>
-    <h1>Groups</h1>
-    <button className="joinGroup">Join Group</button>
-
-
-    <div className="team-container">
-
-    </div>
-  </div>;
+  );
 }
 export default Collaborate;
