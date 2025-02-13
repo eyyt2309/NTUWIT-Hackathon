9a25747176ca8ea5cbac94199d03df9899f33590
CLJR <calvinleejiarong@gmail.com>
2025-02-13 09:52:33 +0800

    backend setup

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,25 @@
-from flask import Flask, request, jsonify
-import requests
-import firebase_admin
-from firebase_admin import credentials
+from flask import Flask, jsonify
+import redis
 
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
 app = Flask(__name__)
-# Flask routing to get latitude and longitude
 
-@app.route('/hello', methods=['GET'])
-def index():
-    return "hello world"
+# Connect to Redis with authentication
+redis_cache = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 
+@app.route('/set/<string:key>/<string:value>', methods=['POST'])
+def set_key(key, value):
+    if not redis_cache.exists(key):
+        redis_cache.set(key, value)
+        return jsonify({"message": "Key set successfully"}), 201  # 201 Created
+    return jsonify({"message": "Key already exists"}), 409  # 409 Conflict
 
-# @app.route('/getCoords', methods=['GET'])
-# def get_coords():
-#     postalcode = request.args.get('postalcode')
-#     coords = getCoords(postalcode)
-#     return jsonify(coords)
+@app.route('/get/<string:key>', methods=['GET'])
+def get_key(key):
+    value = redis_cache.get(key)
+    if value is not None:
+        return jsonify({"key": key, "value": value}), 200  # 200 OK
+    return jsonify({"error": f"Key '{key}' does not exist"}), 404  # 404 Not Found
 
-
-# run on port 5000
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(debug=True)
