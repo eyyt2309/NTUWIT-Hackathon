8b0a62fedb41240c33c3b0f3001930c451f7435f
CLJR <calvinleejiarong@gmail.com>
2025-02-13 23:29:18 +0800

    fixed backend

--- backend/app.py
+++ backend/app.py
@@ -91,18 +91,3 @@ def getinfo():
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-# @app.route('/set/<string:key>/<string:value>', methods=['POST'])
-# def set_key(key, value):
-#     if not redis_cache.exists(key):
-#         redis_cache.set(key, value)
-#         return jsonify({"message": "Key set successfully"}), 201  # 201 Created
-#     return jsonify({"message": "Key already exists"}), 409  # 409 Conflict
-
-# @app.route('/get/<string:key>', methods=['GET'])
-# def get_key(key):
-#     value = redis_cache.get(key)
-#     if value is not None:
-#         return jsonify({"key": key, "value": value}), 200  # 200 OK
-#     return jsonify({"error": f"Key '{key}' does not exist"}), 404  # 404 Not Found
-
